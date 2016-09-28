45618d3df283514f9793538385a982abbb5b3c34
Hackbright Student <info@hackbrightacademy.com>
2016-09-28 16:24:36 -0700

    Included input_calculator function to determine action based on user input

--- calculator.py
+++ calculator.py
@@ -7,5 +7,31 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+while True:
+    input_string = raw_input("> ") #what the user is entering
+    tokens = input_string.split(" ")
+    operator = tokens[0]
+    num1 = int(tokens[1])
+    num2 = int(tokens[2])
+    
 
-# Your code goes here
+def input_calculator(operator, num1, num2):
+    if operator == "q":
+        break
+    elif operator == "+":
+        add(num1, num2)
+    elif operator == "-":
+        subtract(num1, num2)
+    elif operator == "*":
+        multiply(num1, num2)
+    elif operator == "/":
+        divide(num1, num2)
+    elif operator == "square":
+        square(num1)
+    elif operator == "cube":
+        cube(num1)
+    elif operator == "pow":
+        power(num1, num2)
+    elif operator == "mod":
+        mod(num1, num2)
+
