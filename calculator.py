38dee7ff713c7ec012582454f45533c763d23704
Hackbright Student <info@hackbrightacademy.com>
2016-09-28 17:30:38 -0700

    Moved while loop into function and put num variables under each operator based on needs

--- calculator.py
+++ calculator.py
@@ -7,35 +7,52 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-while True:
-    input_string = raw_input("> ") #what the user is entering
-    tokens = input_string.split(" ")
-    operator = tokens[0]
-    num1 = int(tokens[1])
-    num2 = int(tokens[2])
-    input_calculator(operator, num1, num2)
 
-def input_calculator(operator, num1, num2):
+def input_calculator():
     """Calculates user input
 
     Determines and solves mathematical expression based on user input"""
+    while True:
+        input_string = raw_input("> ") #what the user is entering
+        tokens = input_string.split(" ")
+        operator = tokens[0]
 
-    if operator == "q":
-        break
-    elif operator == "+":
-        add(num1, num2)
-    elif operator == "-":
-        subtract(num1, num2)
-    elif operator == "*":
-        multiply(num1, num2)
-    elif operator == "/":
-        divide(num1, num2)
-    elif operator == "square":
-        square(num1)
-    elif operator == "cube":
-        cube(num1)
-    elif operator == "pow":
-        power(num1, num2)
-    elif operator == "mod":
-        mod(num1, num2)
+        if operator == "q":
+            return
+        elif operator == "+":
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+            result = add(num1, num2)
+        elif operator == "-":
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+            result = subtract(num1, num2)
+        elif operator == "*":
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+            result = multiply(num1, num2)
+        elif operator == "/":
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+            result = divide(num1, num2)
+        elif operator == "square":
+            num1 = int(tokens[1])
+            result = square(num1)
+        elif operator == "cube":
+            num1 = int(tokens[1])
+            result = cube(num1)
+        elif operator == "pow":
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+            result = power(num1, num2)
+        elif operator == "mod":
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+            result = mod(num1, num2)
+        print result
+
+input_calculator()
+    
+
+
 
